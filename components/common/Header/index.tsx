9ec08acd01ab2b66ed7d60b4db05c63ce07810f0
hao2001vt <nguyenchihao2001vt@gmail.com>
2022-03-18 09:40:10 +0700

    first commit

---
+++ components/common/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export const Header = () => {
+  return (
+    <Box>
+      <Typography component="h3" variant="h1">
+        Header
+      </Typography>
+    </Box>
+  );
+};
